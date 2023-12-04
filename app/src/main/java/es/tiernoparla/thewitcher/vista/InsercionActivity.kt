c25cf57bfe3b876a415b9057985002081db7cd00
Tu nombre <tu@mail.com>
2023-12-04 11:08:56 +0100

    Peticion de permisos

--- app/src/main/java/es/tiernoparla/thewitcher/vista/InsercionActivity.kt
+++ app/src/main/java/es/tiernoparla/thewitcher/vista/InsercionActivity.kt
@@ -1,7 +1,9 @@
 
 package es.tiernoparla.thewitcher.vista
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -9,6 +11,8 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import es.tiernoparla.thewitcher.Auxiliar
 import es.tiernoparla.thewitcher.databinding.ActivityInsercionBinding
 import es.tiernoparla.thewitcher.modelo.Personaje
@@ -35,6 +39,54 @@ class InsercionActivity : AppCompatActivity() {
         binding=ActivityInsercionBinding.inflate(layoutInflater)
         setContentView(binding.root)
         seleccionarImagen()
+        binding.btnInsertar.setOnClickListener(){
+            verificarPermisos()
+        }
+    }
+
+    /**
+     * Solicitud de permisos
+     */
+    private fun verificarPermisos(){
+        //Permisos concedidos, se prodecerá a insertar
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+            insertarPersonaje()
+        }
+        //Permisos no concedidos aunque tampoco rechazados(no haber preguntado por ellos)
+        else{
+            pedirPermisos()
+        }
+    }
+
+    private fun pedirPermisos() {
+        val msg="Si no acepta los permisos la imagen podría no funcionar correctamente"
+        val msg2="El permiso no ha sido aceptado, para cambiarlo accede a los ajustes de tu dispositivo"
+        //Permisos ya rechazados, igualemnte se insertará el personaje
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(Auxiliar.TIPO_AVISO)
+                .setMessage(msg2)
+                .setPositiveButton(Auxiliar.OK) { _, _ ->
+                   insertarPersonaje()
+                }
+            val dialog: AlertDialog= builder.create()
+            dialog.show()
+        }
+        //Los permisos no estan rechazados, se pregunta si aceptarlos o no
+        else{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(Auxiliar.TIPO_AVISO)
+                .setMessage(msg)
+                .setPositiveButton(Auxiliar.OK) { _, _ ->
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+                }
+            val dialog: AlertDialog= builder.create()
+            dialog.show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         insertarPersonaje()
     }
 
@@ -56,7 +108,6 @@ class InsercionActivity : AppCompatActivity() {
         var msg="Los campos nombre, descripción e imagen no pueden estar vacíos"
         var msg2="¿Añadir Personaje?"
         var msgToast="Personaje insertado"
-        binding.btnInsertar.setOnClickListener(){
             if(binding.etNombreInsertar.text.isNotEmpty() && binding.etDescripcionInsertar.text.isNotEmpty() && binding.tvImgInsertada.text.isNotEmpty()){
                 val builder = AlertDialog.Builder(this)
                 builder.setTitle(Auxiliar.TIPO_AVISO)
@@ -80,7 +131,7 @@ class InsercionActivity : AppCompatActivity() {
             else{
                 Auxiliar.mostrarAviso(this,Auxiliar.TIPO_ERROR,msg)
             }
-        }
+
     }
 
     @Deprecated("Metodo usado sin seguir arquitectura MVVM, reemplazado por insertarPersonaje")
